01b92fe65981cd6fb3d21ac1e5f6a941c9066382
geekychetan <chetan.lohar@enterprisedb.com>
2025-01-20 18:26:08 +0530

    feat(#1): Initialized basic project structure

---
+++ src/main/java/com/geekychetan/springboot_grpc_client/SpringbootGrpcClientApplication.java
@@ -0,0 +1,13 @@
+package com.geekychetan.springboot_grpc_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGrpcClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGrpcClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geekychetan/springboot_grpc_client/SpringbootGrpcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekychetan.springboot_grpc_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGrpcClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
